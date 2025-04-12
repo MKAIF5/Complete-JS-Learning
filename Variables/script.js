9406395592c6d031f9e897804f4eed757b397d16
mohammed kaif <mkaif555123@gmail.com>
2025-04-12 07:27:28 +0500

    variables completed

---
+++ Variables/script.js
@@ -0,0 +1,16 @@
+const accountId = 1234561;
+let accountEmail = "kaif@google.com";
+var accountPassword = 123123123;
+accountCity = "karachi";
+
+
+accountEmail = "kaif@gmail.com";
+accountPassword = 123456789;
+accountCity = "lahore";
+
+// console.table([accountId ,accountEmail ,accountPassword ,accountCity])
+
+// console.log(accountId)
+
+// const: not changed
+// let: block scope and functional scope
