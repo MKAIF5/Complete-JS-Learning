924dff9d160781031d6eda2d37032e9df3cb963a
mohammed kaif <mkaif555123@gmail.com>
2025-04-12 09:04:27 +0500

    part 1 complete

---
+++ DataTypes/script.js
@@ -0,0 +1,23 @@
+"use strict" // javascript use a newer version
+
+// alert(5 + 5);
+
+// Data Types
+
+const name = "kaif";
+const age = 16;
+const isLogged = true;
+
+/*
+
+string => ""
+number => 2 power to 53
+boolean => true/false
+null => standalone
+undefined => 
+symbol => unique
+bigInt => 
+
+*/
+
+// console.log(typeof isLogged);
