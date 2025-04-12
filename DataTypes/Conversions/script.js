6885ee223fcf13ef2fc3738677d19f317951ccd4
mohammed kaif <mkaif555123@gmail.com>
2025-04-12 09:47:35 +0500

    Data Types Part 2 Complete

---
+++ DataTypes/Conversions/script.js
@@ -0,0 +1,20 @@
+let score = true
+
+let convertInString = Number(score) 
+
+// console.log(typeof convertInString);
+// console.log(convertInString);
+
+// Number
+
+
+// "" => 0
+// "33abc" => NaN
+// "33" => 33
+// true => 1; false => 0
+
+const num = 1;
+
+const convertIntoBoolean = Boolean(num);
+
+console.log(convertIntoBoolean);
