f695e56f75a0017a0c6333bb94a1ba30c6924064
mohammed kaif <mkaif555123@gmail.com>
2025-04-13 07:49:32 +0500

    data Types Operations

---
+++ DataTypes/Operations/script.js
@@ -0,0 +1,31 @@
+// Operations
+
+// const value = 3;
+// const negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**3);
+
+// console.log(+"");
+
+// let gameCounter = 100;
+// gameCounter++
+
+// console.log(gameCounter);
+
+// tricky 
+
+// let x = 10;
+// let y = x++;
+
+// console.log(`x ${x} y ${y}`);
+
+// let a = 5;
+// let b = ++a;
+
+// console.log(`a ${a} b ${b}`);
