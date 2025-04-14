e31c60ccdd464e8e587e09d27dd09d9b76b79296
mohammed kaif <mkaif555123@gmail.com>
2025-04-14 16:55:00 +0500

    strings method practice

---
+++ DataTypes/Strings/script.js
@@ -0,0 +1,29 @@
+const myName = "kaif";
+const repoCount = 74;
+// console.log(name + repoCount + "Value");
+
+// console.log(`My Name Is ${myName} My Github Repo Is ${repoCount}`);
+
+const str = new String("kaif-kk-555");
+// console.log(str);
+// console.log(str[3]);
+// console.log(str.length);
+
+// console.log(str.charAt(2));
+// console.log(str.indexOf("i"));
+
+const anotherStr = str.substring(0 , 4);
+// console.log(anotherStr);
+
+const newStr = str.slice(-5 , 4);
+// console.log(newStr);
+
+const newStrOne = "     kaif     ";
+// console.log(newStrOne);
+// console.log(newStrOne.trim());
+
+const url = "https//kaif%20khan.com";
+// console.log(url.replace("%20" , '-'));
+// console.log(url.includes("imran"));
+
+// console.log(str.split("-"));
