6bd6b4bd7d8d564d0d49587dda9cedd0eb61d85d
mohammed kaif <mkaif555123@gmail.com>
2025-04-15 18:48:51 +0500

    Numbers Practice Completed

---
+++ DataTypes/Numbers And Math/script.js
@@ -0,0 +1,19 @@
+// Numbers And Math
+
+
+// Numbers
+const score = 400;
+// console.log(score);
+
+const balance = new Number(500);
+// console.log(balance);
+
+// console.log(balance.toFixed(10));
+// console.log(balance.toString());
+
+// const hundreds = 10000000;
+// console.log(hundreds.toLocaleString());
+
+// Math
+
+// console.log(Math);
