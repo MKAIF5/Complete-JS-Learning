dd7fab1251dc9168e71d25bb7f637091c927b355
mohammed kaif <mkaif555123@gmail.com>
2025-04-16 16:15:21 +0500

    Maths Concept Completed

--- DataTypes/Numbers And Math/script.js
+++ DataTypes/Numbers And Math/script.js
@@ -17,3 +17,16 @@ const balance = new Number(500);
 // Math
 
 // console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(5.9));
+// console.log(Math.floor(4.5653465));
+// console.log(Math.ceil(4.1));
+// console.log(Math.min(4, 5, 6, 2));
+// console.log(Math.max(9,5,3,7,));
+
+// console.log(Math.floor(Math.random() * 10 + 1));
+
+const min = 10;
+const max = 20;
+
+// console.log(Math.floor(Math.random() *( max - min + 1) + min))
