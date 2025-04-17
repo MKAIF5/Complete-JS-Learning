ab4a4b2258f39baaa2b502abe8c2155f8212f54e
mohammed kaif <mkaif555123@gmail.com>
2025-04-17 15:12:12 +0500

    array Basics Completed

---
+++ Arrays/Basics/script.js
@@ -0,0 +1,10 @@
+// Array
+
+const myArr = [1, 2, 3, 4, 5, true, "kaif"]; // mox datatype array
+// const strArr = ["ironman", "thor", 'hulk']; // str array
+
+// const newArray = new Array(1, 2, 3, 4, 5)
+
+// console.log(newArray);
+
+myArr.push(8)
