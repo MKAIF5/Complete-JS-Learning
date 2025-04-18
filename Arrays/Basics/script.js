966cfb2dae2ed7b67b4a00a4c9a5aa04ed5152c3
mohammed kaif <mkaif555123@gmail.com>
2025-04-18 14:57:25 +0500

    Array Part 1 Completed

--- Arrays/Basics/script.js
+++ Arrays/Basics/script.js
@@ -1,10 +1,34 @@
 // Array
 
-const myArr = [1, 2, 3, 4, 5, true, "kaif"]; // mox datatype array
+// const myArr = [1, 2, 3, 4, 5, true, "kaif"]; // mix datatype array
 // const strArr = ["ironman", "thor", 'hulk']; // str array
 
 // const newArray = new Array(1, 2, 3, 4, 5)
 
 // console.log(newArray);
 
-myArr.push(8)
+// Array Methods
+
+// const newArr1 = myArr.includes(9);
+// console.log(newArr1);
+
+// const newArr2 = myArr.indexOf(7);
+// console.log(myArr);
+
+const arr = [0, 1, 2, 3, 4, 5];
+// console.log(arr.join());
+// console.log(arr);
+
+// slice and splice
+
+console.log("A", arr);
+
+const sliceArr = arr.slice(1, 3);
+console.log(sliceArr);
+
+console.log("B", arr);
+
+const spliceArr = arr.splice(1, 3)
+console.log(spliceArr);
+
+console.log("C" , arr);
