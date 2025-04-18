7e0d2d7080511dd257b20a36259a73dbf2c87d20
mohammed kaif <mkaif555123@gmail.com>
2025-04-18 18:58:51 +0500

    part 2 completed array

---
+++ Arrays/Basics 2/script.js
@@ -0,0 +1,8 @@
+const marvel_heros = ["ironman" , "thor" , "hulk"];
+const dc_heros = ["superman" , "batman" , "flash"];
+
+// const newArr = marvel_heros.concat(dc_heros);
+// console.log(newArr);
+
+// const anotherArr = [...marvel_heros , ...dc_heros];
+// console.log(anotherArr);
