5d834516fbf7a3fbe9e09538ab06ff3261670148
mohammed kaif <mkaif555123@gmail.com>
2025-04-24 18:44:52 +0500

    hoisting and scope all completed

--- Global Scope Vs Local Scope/script.js
+++ Global Scope Vs Local Scope/script.js
@@ -1,15 +1,61 @@
 // global and local scope
 
+// basics
+
 var a = 300;
 
 if (true) {
     let a = 10;
     const b = 20;
     c = 30;
-    console.log(a);
+    // console.log(a);
 }
 
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+// functions
+
+function one() {
+    const username = "kaif";
+    function two() {
+        const website = "youtube"
+        // console.log(username);
+    };
+    // console.log(website);
+    two()
+}
+one()
+
+// if else
+
+if (true) {
+    const username = "kaif";
+    if (username === "kaif") {
+        const website = "youtube"
+        // console.log(website);   
+    }
+    // console.log(username);
+
+    // console.log(website);
+}
+
+// console.log(username);
+
+// +++++++++++++++++++ intersting +++++++++++++++++++ 
+
+// hoisting
+
+function addOne(num) {
+    return num + 1
+};
+
+console.log(addOne(5));
+
+const addTwo = function (num) {
+    return num + 2
+};
+
+console.log(addTwo(5));
