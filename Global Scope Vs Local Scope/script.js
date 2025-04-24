88589d3c218910863a3d9dfc357432108e5e472b
mohammed kaif <mkaif555123@gmail.com>
2025-04-24 16:52:50 +0500

    global and local scope

---
+++ Global Scope Vs Local Scope/script.js
@@ -0,0 +1,15 @@
+// global and local scope
+
+var a = 300;
+
+if (true) {
+    let a = 10;
+    const b = 20;
+    c = 30;
+    console.log(a);
+}
+
+
+// console.log(a);
+// console.log(b);
+console.log(c);
