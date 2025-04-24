e928169c7e3bf3b0eb96e51534e2b40075adbe57
mohammed kaif <mkaif555123@gmail.com>
2025-04-24 14:55:57 +0500

    object basic 2 completed

---
+++ Objects/Basic 2/script.js
@@ -0,0 +1,65 @@
+// object singleTon , constructor
+
+// const obj = new Object();
+
+// object literals
+
+const obj = {};
+
+obj.name = "kaif khan",
+    obj.id = 123;
+obj.isLoggedIn = false
+
+// console.log(obj);
+
+const obj1 = {
+    1: "a",
+    2: "b",
+    3: "c"
+}
+
+const obj2 = {
+    4: "d",
+    5: "e",
+    6: "f"
+}
+
+const obj3 = Object.assign({} , obj1 , obj2);
+// console.log(obj3);
+
+
+const regularUser = {
+    email: "kaif@google.com",
+    fullname: {
+        username:{
+            firstname: "kaif", 
+            lastname: "khan"
+        }
+    }
+};
+// console.log(regularUser.fullname.username.lastname);
+
+const user = [
+    {
+        email: "kaif@gmail.com",
+        id: 1
+    },
+    {
+        email: "kaif@gmail.com",
+        id: 2
+    },
+    {
+        email: "kaif@gmail.com",
+        id: 3
+    },
+    {
+        email: "kaif@gmail.com",
+        id: 4
+    },
+    {
+        email: "kaif@gmail.com",
+        id: 5
+    },
+];
+
+const arrayAccess =   user[1].email;
