f456f5ef67739926e5530ca6ef5c83577befb786
mohammed kaif <mkaif555123@gmail.com>
2025-04-25 15:36:12 +0500

    IIFE Completed

---
+++ Functions/IIFE/script.js
@@ -0,0 +1,13 @@
+// IIFE : Immediately Invoked Function Expression
+
+(function kaif() {
+    console.log("DB Connected");
+})();
+
+(() => {
+    console.log("DB Connected two");
+})();
+
+(() => {
+    console.log("DB Connected three");
+})()
