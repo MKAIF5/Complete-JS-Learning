166cf03d756ed76a4fbdf29107e3bfd30e13c3d1
mohammed kaif <mkaif555123@gmail.com>
2025-04-30 02:09:58 +0500

    do while and while completed

---
+++ While And Do While Loop/script.js
@@ -0,0 +1,29 @@
+// while and do while loop
+
+
+
+// while
+let i = 1;
+
+while (i <= 10) {
+    // console.log(i);
+    i++
+}
+
+let arr = ["flash", "batman", "superman"];
+let j = 0;
+
+while (j < arr.length) {
+    // console.log(arr[j]);
+    j++
+}
+
+// do while
+
+let k = 1;
+
+do {
+    // console.log(k);
+    k++
+
+} while (k <= 10);
