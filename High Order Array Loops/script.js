6649674319cb6886599de81488e4003883e77f02
mohammed kaif <mkaif555123@gmail.com>
2025-04-30 16:06:36 +0500

    For in And For of ompleted

---
+++ High Order Array Loops/script.js
@@ -0,0 +1,63 @@
+// for of 
+
+// array
+
+const arr = [1, 2, 3, 4, 5];
+
+for (const val of arr) {
+    // console.log(val);
+}
+
+// strings
+
+const greetings = "Hello World";
+
+for (const greet of greetings) {
+
+    // console.log(`The Char Is ${greet}`);
+};
+
+// Maps
+
+const map = new Map();
+
+map.set("Pakistan", "Islamabad");
+map.set("India", "Delhi");
+map.set("Japan", "Tokyo");
+
+for (const [keys, values] of map) {
+    //  console.log(keys , values);
+};
+
+// objects
+
+// const myObject = {
+//     name: "kaif",
+//     age: "16"
+// };
+
+// for (const obj of myObject) {
+//     // console.log(obj);
+// }
+
+// for in
+
+// objects
+
+const language = {
+    js: "javascript",
+    py: "python",
+    cpp: "c++"
+}
+
+for (const key in language) {
+//    console.log(`${key} And The Full Name Is ${language[key]}`);
+}
+
+// arrays 
+
+const coding = ["js" , "cpp" , "py" , "java"];
+
+for (const key in coding) {
+   console.log(key);
+}
