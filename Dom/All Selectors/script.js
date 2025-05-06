8c9f10e413444962c210d80cc5e4f30a89d11a40
mohammed kaif <mkaif555123@gmail.com>
2025-05-06 16:54:34 +0500

    dom selectors done

---
+++ Dom/All Selectors/script.js
@@ -0,0 +1,19 @@
+const title = document.getElementById("title");
+const heading = document.getElementsByClassName("heading");
+
+title.style.backgroundColor = "green";
+title.style.color = "white";
+title.style.padding = "10px"
+title.style.borderRadius = "10px"
+
+const innerText =  title.innerText
+const innerHtml = title.innerHTML
+const textContent = title.textContent
+
+// console.log(innerText);
+// console.log(textContent);
+// console.log(innerHtml);
+
+
+// console.log(heading);
+// console.log(title);
